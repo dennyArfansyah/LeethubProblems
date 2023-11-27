306c2ba32f168036b480addac3de49cda5922257
Denny Arfansyah <dennyarfansyah@gmail.com>
2023-11-27 17:34:01 +0700

    Time: 42 ms, Memory: 15 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
@@ -2,10 +2,7 @@ class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
         var newNums = Set<Int>()
         newNums.formUnion(nums.sorted())
-        nums.removeAll()
-        for num in newNums.sorted() {
-            nums.append(num)
-        }
+        nums = Array(newNums.sorted())
         return nums.count
     }
 }
