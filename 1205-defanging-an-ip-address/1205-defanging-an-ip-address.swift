d08b9cf3f60ff248b831b9b01a10da68820e047a
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-11 17:54:49 +0700

    Time: 7 ms (14.63%), Space: 19.3 MB (15.85%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.swift
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.swift
@@ -1,9 +1,5 @@
 class Solution {
     func defangIPaddr(_ address: String) -> String {
-        var result = ""
-        for adrs in address {
-            result += (adrs == ".") ? "[.]" : String(adrs)
-        }
-        return result
+        return address.replacingOccurrences(of: ".", with: "[.]")
     }
 }
