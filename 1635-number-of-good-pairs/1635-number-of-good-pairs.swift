b27e5e897ed7c43b754dbca069755edddd8b765c
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-11 17:50:44 +0700

    Time: 0 ms (100%), Space: 19.7 MB (42.86%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.swift
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.swift
@@ -1,20 +1,13 @@
 class Solution {
     func numIdenticalPairs(_ nums: [Int]) -> Int {
-        var result = 0
-        var numsJ = nums
-        
-        for i in 0 ... nums.count-1 {
-            if numsJ.count > 1 {
-                for j in 1 ... numsJ.count-1 {
-                    if nums[i] == numsJ[j] {
-                        print(nums[i], numsJ[j])
-                        result += 1
-                    }
+        var count = 0
+        for (i, num) in nums.enumerated() {
+            for (j, n) in nums.enumerated() {
+            if nums[i] == nums[j] && i < j {
+                    count += 1
                 }
-                numsJ.remove(at: 0)
             }
         }
-        
-        return result
+        return count
     }
 }
