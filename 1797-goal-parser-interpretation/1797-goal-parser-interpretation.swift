b139bb07db2e4ee548cb61418463e01d12d8ba07
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-13 23:30:14 +0700

    Time: 4 ms (64.29%), Space: 19.2 MB (28.57%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.swift
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.swift
@@ -1,13 +1,6 @@
 class Solution {
     func interpret(_ command: String) -> String {
-        var result = command
-        if result.contains("()") {
-            result = result.replacingOccurrences(of: "()", with: "o")
-        }
-        
-        if result.contains("(al)") {
-            result = result.replacingOccurrences(of: "(al)", with: "al")
-        }
-        return result
+        var result = command.replacingOccurrences(of: "()", with: "o")
+        return result.replacingOccurrences(of: "(al)", with: "al")
     }
 }
