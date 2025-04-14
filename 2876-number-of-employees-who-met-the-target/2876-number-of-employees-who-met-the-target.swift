066f57cf0efdfbc47166797bf7a032ab8bf41512
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-14 21:01:33 +0700

    Time: 3 ms (0%), Space: 19.3 MB (76.19%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.swift
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.swift
@@ -1,11 +1,5 @@
 class Solution {
     func numberOfEmployeesWhoMetTarget(_ hours: [Int], _ target: Int) -> Int {
-        var result = 0
-        for hour in hours {
-            if hour >= target {
-                result += 1
-            }
-        }
-        return result
+        hours.filter { $0 >= target }.count
     }
 }
