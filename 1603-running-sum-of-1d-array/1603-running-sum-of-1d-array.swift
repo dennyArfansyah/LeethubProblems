cbf6d3e3ecd52d5b5d5a51e220f701db6291e637
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-14 21:25:50 +0700

    Time: 0 ms (100%), Space: 19.7 MB (63.6%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var result: [Int] = []
+        var sum = 0
+        for num in nums {
+            sum += num
+            result.append(sum)
+        }
+        return result
+    }
+}
