84b04d5ab972b7aefcf32a4ed7fa3d5500134141
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-14 21:41:24 +0700

    Time: 5 ms (70.97%), Space: 19.8 MB (77.42%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.swift
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.swift
@@ -1,11 +1,5 @@
 class Solution {
     func mostWordsFound(_ sentences: [String]) -> Int {
-        var result = 0
-        for sentence in sentences {
-            if sentence.components(separatedBy: " ").count > result {
-                result = sentence.components(separatedBy: " ").count
-            }
-        }
-        return result
+        sentences.map { $0.split(separator: " ").count }.max() ?? 0
     }
 }
