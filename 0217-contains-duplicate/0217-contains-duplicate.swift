5342cad39744c849d66261b4edfd18bb471437d0
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-21 12:06:52 +0700

    Time: 24 ms (8.34%), Space: 22.2 MB (61.23%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var fruits: Set<Int> = []
+        nums.forEach { n in
+            fruits.insert(n)
+        }
+        return fruits.count == nums.count ? false : true
+    }
+}
