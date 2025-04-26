0b7e34d2fb2dc66031ab7551b7554d6f6ed931d5
Denny Arfansyah <dennyarfansyah@gmail.com>
2025-04-27 02:04:50 +0700

    Time: 0 ms (100%), Space: 19.4 MB (77.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.swift
+++ 0088-merge-sorted-array/0088-merge-sorted-array.swift
@@ -1,10 +1,14 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var i = m
-    for j in 0..<n {
-        nums1[i] = nums2[j]
-        i += 1
-    }
-    nums1.sort()
+        var newNums1 = [Int]()
+        for i in 0..<m {
+            newNums1.append(nums1[i])
+        }
+        for i in 0..<n {
+            newNums1.append(nums2[i])
+        }
+        
+        nums1.removeAll()
+        nums1 = newNums1.sorted()
     }
 }
